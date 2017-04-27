522c5286d4b478016429770bd2faeaeca7849e76
LucianoFromTrelew <lucianoserruya@hotmail.com>
2017-04-27 18:48:50 -0300

    Cabeceras de las operaciones del ADT Lista

--- TPs/tp2/ej9/adt_lo/listas.h
+++ TPs/tp2/ej9/adt_lo/listas.h
@@ -1,6 +1,21 @@
 #include "../personal.h"
 
-typedef struct{
-   Personal *lista;
-   int cant;
-}
+
+typedef tipoClave   int;
+typedef tipoInfo    int;       /*A MODIFICAR SEGÚN NECESIDAD*/
+
+typedef tipoLista *t_lista;
+typedef int(*comparar)(const void*, const void*);
+
+int crear_lista(tipoLista, comparar);
+int insertar_lista(tipoLista, tipoClave, tipoInfo);
+int eliminar_lista(tipoLista, tipoClave);
+int longitud_lista(tipoLista);
+int recuClave_lista(tipoLista, tipoClave, tipoInfo*);
+int recuAnt_lista(tipoLista, tipoClave, tipoClave*);
+int recuSig_lista(tipoLista, tipoClave, tipoClave*);
+int recuPrim_lista(tipoLista);
+int recuUlt_lista(tipoLista, tipoClave*);
+int esVacia_lista(tipoLista);
+int vaciarLista_lista(tipoLista);
+
